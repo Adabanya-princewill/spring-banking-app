7486a7d850903e6442d018d1e2ff4eb9e2d4ea67
Princewill-Adabanya <princewill.adabanya@novabank.ng>
2025-06-08 17:05:26 +0100

    removed swagger

--- src/main/java/com/example/dto/springapp/SpringappApplication.java
+++ src/main/java/com/example/dto/springapp/SpringappApplication.java
@@ -1,19 +1,9 @@
 package com.example.dto.springapp;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-@OpenAPIDefinition(
-		info = @Info(
-				title = "My Bank API",
-				version = "1.0",
-				description = "API documentation for bank app"
-		)
-)
 public class SpringappApplication {
 
 	public static void main(String[] args) {
